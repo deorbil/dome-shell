f3a0fb2ba32f6540b4c1bf32d3f156182f18cf93
deorbil <80085032+deorbil@users.noreply.github.com>
2025-06-05 22:27:11 +0700

    chore: set typescript base url

--- app.tsx
+++ app.tsx
@@ -1,7 +1,7 @@
 #!/usr/bin/gjs -m
 import { App } from "astal/gtk3";
-import styles from "./styles/main.scss";
-import { Bar } from "./widgets";
+import styles from "styles/main.scss";
+import { Bar } from "widgets";
 
 App.start({
   css: styles,
