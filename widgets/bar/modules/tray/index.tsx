fd682faf2386f772d712631c075a458b6bbec75a
deorbil <80085032+deorbil@users.noreply.github.com>
2025-06-08 08:11:16 +0700

    fix(bar): tray menu not destroyed and variable not dropped

--- widgets/bar/modules/tray/index.tsx
+++ widgets/bar/modules/tray/index.tsx
@@ -5,37 +5,39 @@ import AstalTray from "gi://AstalTray";
 const tray = AstalTray.get_default();
 
 function TrayItem({ item }: { item: AstalTray.TrayItem }) {
-  const menu = Variable.derive(
+  let menu: Gtk.Menu;
+
+  const menuModel = Variable.derive(
     [bind(item, "menuModel"), bind(item, "actionGroup")],
     (menuModel, actionGroup) => {
-      const menu = Gtk.Menu.new_from_model(menuModel);
+      menu?.destroy();
+      menu = Gtk.Menu.new_from_model(menuModel);
       menu.insert_action_group("dbusmenu", actionGroup);
       menu.widthRequest = 220;
-      return {
-        activate() {
-          item.activate(0, 0);
-        },
-        show(widget: Gtk.Widget) {
-          menu.popup_at_widget(
-            widget,
-            Gdk.Gravity.SOUTH,
-            Gdk.Gravity.NORTH,
-            null,
-          );
-        },
-      };
     },
   );
+
+  function activate() {
+    item.activate(0, 0);
+  }
+
+  function show(self: Gtk.Widget) {
+    self.set_state(Gtk.StateType.NORMAL);
+    menu?.popup_at_widget(self, Gdk.Gravity.SOUTH, Gdk.Gravity.NORTH, null);
+  }
 
   return (
     <button
       onClickRelease={(self, e) => {
         if (e.button === Astal.MouseButton.PRIMARY) {
-          menu.get().activate();
+          activate();
         } else if (e.button === Astal.MouseButton.SECONDARY) {
-          self.set_state(Gtk.StateType.NORMAL);
-          menu.get().show(self);
+          show(self);
         }
+      }}
+      onDestroy={() => {
+        menu?.destroy();
+        menuModel.drop();
       }}
     >
       <icon gicon={bind(item, "gicon")} />
