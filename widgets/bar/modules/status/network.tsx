6ce338cf493b56057011ce49161e26f528353075
deorbil <80085032+deorbil@users.noreply.github.com>
2025-06-16 09:07:18 +0700

    feat(bar): add wired network status indicator

--- widgets/bar/modules/status/network.tsx
+++ widgets/bar/modules/status/network.tsx
@@ -1,13 +1,39 @@
-import { bind } from "astal";
+import { bind, Variable } from "astal";
 import AstalNetwork from "gi://AstalNetwork";
 
 const network = AstalNetwork.get_default();
-const wifi = network.wifi;
+const { wifi, wired } = network;
 
 export function Network() {
+  const visible = Variable.derive(
+    [bind(network, "primary"), bind(wifi, "activeConnection").as(Boolean)],
+    (primary, activeConnection) =>
+      primary === AstalNetwork.Primary.WIFI
+        ? activeConnection
+        : primary === AstalNetwork.Primary.WIRED
+          ? true
+          : false,
+  );
+
+  const iconName = Variable.derive(
+    [bind(network, "primary"), bind(wifi, "iconName"), bind(wired, "iconName")],
+    (primary, wifiIconName, wiredIconName) =>
+      primary === AstalNetwork.Primary.WIFI
+        ? wifiIconName
+        : primary === AstalNetwork.Primary.WIRED
+          ? wiredIconName
+          : undefined,
+  );
+
   return (
-    <button visible={bind(wifi, "activeConnection").as(Boolean)}>
-      <icon icon={bind(wifi, "iconName")} />
+    <button
+      visible={visible()}
+      onDestroy={() => {
+        visible.drop();
+        iconName.drop();
+      }}
+    >
+      <icon icon={iconName()} />
     </button>
   );
 }
